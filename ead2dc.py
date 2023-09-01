8f0bd4b43127234a555a91ca3b410ecf8de599ca
Stephen Davison <sdavison@caltech.edu>
2023-09-01 15:36:09 -0700

    First complete script

--- ead2dc.py
+++ ead2dc.py
@@ -1,19 +1,91 @@
-#Next four lines pull xml from url and save to file; only needed to update the local file; comment out after first run
-import requests
-url = 'https://collections.archives.caltech.edu/oai?verb=GetRecord&identifier=/repositories/2/resources/124&metadataPrefix=oai_ead'
-response = requests.get(url)
-with open('aspace.xml', 'wb') as file:
-   file.write(response.content)
+#Next five lines pull xml from url and save to file; only needed to update the local file; comment out after first run
+#import requests
+#url = 'https://collections.archives.caltech.edu/oai?verb=GetRecord&identifier=/repositories/2/resources/124&metadataPrefix=oai_ead'
+#response = requests.get(url)
+#with open('aspace.xml', 'wb') as file:
+#   file.write(response.content)
 
 import xml.etree.ElementTree as ET
-import xml.dom.minidom
+import xml.dom.minidom as dom
+from datetime import date
+
+today = date.today()
 
 def prettify(elem):
     """Return a pretty-printed XML string for the Element."""
     xml_string = ET.tostring(elem)
-    xml_file = xml.dom.minidom.parseString(xml_string)
+    xml_file = dom.parseString(xml_string)
     pretty_xml = xml_file.toprettyxml(indent="  ")
     return pretty_xml
+
+def buildrecordxml(listrecords, c, collectiontitle, inheriteddata):
+    record = ET.SubElement(listrecords, 'oai:record')
+    header = ET.SubElement(record, 'oai:header')
+    identifier = ET.SubElement(header, 'oai:identifier')
+    identifier.text = 'oai:archives.caltech.edu:' + c.attrib['id']
+    datestamp = ET.SubElement(header, 'oai:datestamp')
+    datestamp.text = today.strftime("%Y-%m-%d")
+    metadata = ET.SubElement(record, 'oai:metadata')
+    dc = ET.SubElement(metadata, 'oai_dc:dc', {'xmlns:oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+                                           'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
+                                           'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+                                           'xsi:schemaLocation': 'http://www.openarchives.org/OAI/2.0/oai_dc/ http:// www.openarchives.org/OAI/2.0/oai_dc.xsd'})
+    for unittitle in c.findall('.//unittitle', ns):
+        title = ET.SubElement(dc, 'dc:title')
+        title.text = unittitle.text
+    title = ET.SubElement(dc, 'dc:title')
+    title.text = collectiontitle
+    if inheriteddata['seriestitle'] is not None:
+        title = ET.SubElement(dc, 'dc:title')
+        title.text = inheriteddata['seriestitle']
+    if inheriteddata['subseriestitle'] is not None:
+        title = ET.SubElement(dc, 'dc:title')
+        title.text = inheriteddata['subseriestitle']
+    for creat in c.findall('.//origination/persname', ns):
+        creator = ET.SubElement(dc, 'dc:creator')
+        creator.text = creat.text
+        creator.attrib = {'scheme': creat.attrib['source']}
+    for creat in c.findall('.//origination/corpname', ns):
+        creator = ET.SubElement(dc, 'dc:creator')
+        creator.text = creat.text
+        creator.attrib = {'scheme': creat.attrib['source']}
+    for unitdate in c.findall('.//unitdate', ns):
+        date = ET.SubElement(dc, 'dc:date')
+        date.text = unitdate.text
+    #type = ET.SubElement(dc, 'dc:type')
+    #type.text = 'type...'
+    for subj in c.findall('.//controlaccess/subject', ns):
+        subject = ET.SubElement(dc, 'dc:subject')
+        subject.text = subj.text
+        subject.attrib = {'scheme': subj.attrib['source']}
+    for geog in c.findall('.//controlaccess/geogname', ns):
+        subject = ET.SubElement(dc, 'dc:subject')
+        subject.text = geog.text
+        subject.attrib = {'scheme': geog.attrib['source']}
+    for pers in c.findall('.//controlaccess/persname', ns):
+        subject = ET.SubElement(dc, 'dc:subject')
+        subject.text = pers.text
+        subject.attrib = {'scheme': pers.attrib['source']}
+    for corp in c.findall('.//controlaccess/corpname', ns):
+        subject = ET.SubElement(dc, 'dc:subject')
+        subject.text = corp.text
+        subject.attrib = {'scheme': corp.attrib['source']}
+    for func in c.findall('.//controlaccess/function', ns):
+        subject = ET.SubElement(dc, 'dc:subject')
+        subject.text = func.text
+        subject.attrib = {'scheme': func.attrib['source']}
+    # description = ET.SubElement(dc, 'dc:description')
+    #description.text = 'description...'
+    for unitid in c.findall('.//unitid', ns):
+        identifier = ET.SubElement(dc, 'dc:identifier')
+        identifier.text = unitid.text
+    for daoloc in c.findall('.//daoloc', ns):
+        identifier = ET.SubElement(dc, 'dc:identifier')
+        # print(daoloc.attrib)
+        identifier.text = daoloc.attrib['{http://www.w3.org/1999/xlink}href']
+        identifier.attrib = {'scheme': 'URI'}
+    return listrecords
+
 
 ns = {'': 'urn:isbn:1-931666-22-9', 'xlink': 'http://www.w3.org/1999/xlink',
       'xsi': "http://www.w3.org/2001/XMLSchema-instance"}
@@ -27,6 +99,7 @@ root = tree.getroot()
 ead = root.findall('.//ead', ns)[0]
 archdesc = ead.findall('.//archdesc', ns)[0]
 dsc = archdesc.findall('.//dsc', ns)[0]
+collectiontitle = archdesc.findall('.//did/unittitle', ns)[0].text
 
 from xml.dom import minidom
 
@@ -57,41 +130,43 @@ schema.text = "http://www.openarchives.org/OAI/2.0/oai_dc.xsd"
 metadataNamespace = ET.SubElement(metadataFormat, 'oai:metadataNamespace')
 metadataNamespace.text = "http://www.openarchives.org/OAI/2.0/oai_dc/"
 
-ListRecords = ET.SubElement(oaixml, 'ListRecords', {'metadataPrefix': 'oai_ead'})
-for i, did in enumerate(dsc.findall('.//*[@level="file"]/did', ns)):
-    record = ET.SubElement(ListRecords, 'oai:record')
-    header = ET.SubElement(record, 'oai:header')
-    identifier = ET.SubElement(header, 'oai:identifier')
-    datestamp = ET.SubElement(header, 'oai:datestamp')
-    metadata = ET.SubElement(record, 'oai:metadata')
-    dc = ET.SubElement(metadata, 'oai_dc:dc', {'xmlns:oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
-                                 'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
-                                 'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-                                 'xsi:schemaLocation': 'http://www.openarchives.org/OAI/2.0/oai_dc/ http:// www.openarchives.org/OAI/2.0/oai_dc.xsd'})
-    for unittitle in did.findall('.//unittitle', ns):
-        title = ET.SubElement(dc, 'dc:title')
-        title.text = unittitle.text
-    creator = ET.SubElement(dc, 'dc:creator')
-    creator.text = 'creator...'
-    for unitdate in did.findall('.//unitdate', ns):
-        date = ET.SubElement(dc, 'dc:date')
-        date.text = unitdate.text
-    type = ET.SubElement(dc, 'dc:type')
-    type.text = 'type...'
-    subject = ET.SubElement(dc, 'dc:subject')
-    subject.text = 'subject...'
-    description = ET.SubElement(dc, 'dc:description')
-    description.text = 'description...'
-    for unitid in did.findall('.//unitid', ns):
-        identifier = ET.SubElement(dc, 'dc:identifier')
-        identifier.text = unitid.text
-    for daoloc in did.findall('.//daoloc', ns):
-        relation = ET.SubElement(dc, 'dc:relation')
-        #print(daoloc.attrib)
-        relation.text = daoloc.attrib['{http://www.w3.org/1999/xlink}href']
-    
+ListRecords = ET.SubElement(oaixml, 'ListRecords', {
+                            'metadataPrefix': 'oai_ead'})
+
+for c in dsc.findall('.//*[@level="series"]', ns):
+    inheriteddata = {'seriestitle': None, 'subseriestitle': None, 'filetitle': None}
+    if c.find('.//unittitle', ns) is not None:
+        seriestitle = c.find('.//unittitle', ns).text
+    else:
+        seriestitle = '[Untitled]'
+    #print('Series:', seriestitle)
+    inheriteddata['seriestitle'] = seriestitle
+    if c.findall('.//*[@level="subseries"]', ns):
+        for c2 in c.findall('.//*[@level="subseries"]', ns):
+            if c2.find('.//unittitle', ns) is not None:
+                subseries = c2.find('.//unittitle', ns).text
+            else:
+                subseries = '[Untitled]'
+            #print('--> Subseries:', subseries)
+            inheriteddata['subseriestitle'] = subseries
+            for c3 in c2.findall('.//*[@level="file"]', ns):
+                if c3.find('.//unittitle', ns) is not None:
+                    filetitle = c3.find('.//unittitle', ns).text
+                else:
+                    filetitle = '[Untitled]'
+                #print('----> Title:', filetitle)
+                inheriteddata['filetitle'] = filetitle
+                ListRecords = buildrecordxml(ListRecords, c3, collectiontitle, inheriteddata)
+    else:
+        for c4 in c.findall('.//*[@level="file"]', ns):
+            if c4.find('.//unittitle', ns) is not None:
+                filetitle = c4.find('.//unittitle', ns).text
+            else:
+                filetitle = '[Untitled]'
+            #print('----> Title:', filetitle)
+            inheriteddata['filetitle'] = filetitle
+            ListRecords = buildrecordxml(ListRecords, c4, collectiontitle, inheriteddata)
 
 
 
 print(prettify(oaixml))
-print('record count:', i+1)
