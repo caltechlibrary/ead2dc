0e2405929ba77e21abdd63bd67f143ae9fae7dba
Stephen Davison <sdavison@caltech.edu>
2023-09-01 16:13:02 -0700

    Glaser, Hale, MacCready added

--- ead2dc.py
+++ ead2dc.py
@@ -1,9 +1,11 @@
-#Next five lines pull xml from url and save to file; only needed to update the local file; comment out after first run
-#import requests
+#This code block pulls xml from url and save to file; only needed to update the local file; comment out after first run
+import requests
 #url = 'https://collections.archives.caltech.edu/oai?verb=GetRecord&identifier=/repositories/2/resources/124&metadataPrefix=oai_ead'
-#response = requests.get(url)
-#with open('aspace.xml', 'wb') as file:
-#   file.write(response.content)
+#url = 'https://collections.archives.caltech.edu/oai?verb=GetRecord&identifier=/repositories/2/resources/197&metadataPrefix=oai_ead'
+url = 'https://collections.archives.caltech.edu/oai?verb=GetRecord&identifier=/repositories/2/resources/34&metadataPrefix=oai_ead'
+response = requests.get(url)
+with open('aspace.xml', 'wb') as file:
+   file.write(response.content)
 
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as dom
