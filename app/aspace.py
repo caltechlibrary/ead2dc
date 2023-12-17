a12390d9c84ec5f132c3a3f19df56cef1bbd7dfb
Stephen Davison <sdavison@caltech.edu>
2023-12-17 14:07:25 -0800

    aspace update

--- app/aspace.py
+++ app/aspace.py
@@ -16,6 +16,7 @@ print(client.get('schemas/sub_container').json())
 print()
 print(list(client.get('schemas').json().keys()))
 print()
-print(client.get('/search', params={'q':'primary_type:resource', 'page':1}).json())
+resource = client.get('repositories/2/archival_objects/106628')
+print(resource)
 
 
