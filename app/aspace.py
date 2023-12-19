453a0f5c2bb0fea37091ae2a34cca439a858486d
Stephen Davison <sdavison@caltech.edu>
2023-12-19 12:39:45 -0800

    aspace update

--- app/aspace.py
+++ app/aspace.py
@@ -10,28 +10,13 @@ client = ASnakeClient(baseurl=config['ArchivesSpace Credentials']['ASPACE_API_UR
                       username=config['ArchivesSpace Credentials']['ASPACE_USERNAME'],
                       password=config['ArchivesSpace Credentials']['ASPACE_PASSWORD'])
 client.authorize()
-repos=client.get('repositories').json()
 
-#print(client.get('schemas/sub_container').json())
-#print()
-#print(list(client.get('schemas').json().keys()))
-#print()
-resource = client.get('repositories/2/archival_objects/106628').json()
-#print(resource)
-#print(resource['title'])
-#print(client.get('repositories/2/resources/219').json())
-#print()
-#resources = list()
-#i = 0
-#for resource in client.get_paged('repositories/2/archival_objects'):
-#    print(resource['title'])
-#    resources.append(resource)
-#    i += 1
-#print(resources[:5])
-#print(len(resources))
-#print(i)
-#print('-----------------')
-print(resource['title'], resource['level'], resource['uri'], resource['resource'])
-for ancestor in resource['ancestors']:
-    ref = client.get(ancestor['ref']).json()
-    print(ref['title'], ref['level'], ref['uri'], resource['resource'])
+i = 0
+for resource in client.get_paged('repositories/2/archival_objects'):
+    i += 1
+print(i)
+
+#print(resource['title'], resource['level'], resource['uri'], resource['resource'])
+#for ancestor in resource['ancestors']:
+#    ref = client.get(ancestor['ref']).json()
+#    print(ref['title'], ref['level'], ref['uri'], resource['resource'])
