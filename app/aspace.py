8a932bc1d75e1f2bd51ee4487d6dacb946fb9159
Stephen Davison <sdavison@caltech.edu>
2023-12-20 15:08:15 -0800

    update aspace.py

--- app/aspace.py
+++ app/aspace.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import time
 
 # read config file
 with open(Path(Path(__file__).resolve().parent).joinpath('config.json'), "r") as f:
@@ -11,12 +12,46 @@ client = ASnakeClient(baseurl=config['ArchivesSpace Credentials']['ASPACE_API_UR
                       password=config['ArchivesSpace Credentials']['ASPACE_PASSWORD'])
 client.authorize()
 
-i = 0
-for resource in client.get_paged('repositories/2/archival_objects'):
-    i += 1
-print(i)
+#i = 0
+#for resource in client.get_paged('repositories/2/archival_objects'):
+#    i += 1
+#print(i)
 
 #print(resource['title'], resource['level'], resource['uri'], resource['resource'])
 #for ancestor in resource['ancestors']:
 #    ref = client.get(ancestor['ref']).json()
 #    print(ref['title'], ref['level'], ref['uri'], resource['resource'])
+
+start = time.time()
+
+colls = list()
+n = 0
+colls = dict()
+for obj in client.get_paged('repositories/2/digital_objects'):
+    try:
+        for c in obj['collection']:
+            if c['ref'][16:26] != 'accessions':
+                if c['ref'] in colls.keys():
+                    colls[c['ref']] = colls[c['ref']] + 1
+                else:
+                    colls[c['ref']] = 1
+                n += 1
+    except:
+        try:
+            c = obj['resource']['ref']
+            if c['ref'][16:26]!='accessions':
+                if c['ref'] in colls.keys():
+                    colls[c['ref']] = colls[c['ref']] + 1
+                else:
+                    colls[c['ref']] = 1
+                n += 1
+        except:
+            print('failed')
+for key in colls:
+    print(client.get(key).json()['title'], '(coll. numb.:', key[26:] + ', count:', str(colls[key]) + ')')
+print('number of digital objects:', n)
+print('number of collections:', len(colls))
+
+end = time.time()
+delta = end - start
+print('time:', delta, 'seconds')
