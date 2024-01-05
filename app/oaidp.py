f3485215b4d8082899e57feec92b800aa8018783
Stephen Davison <sdavison@caltech.edu>
2024-01-04 16:00:22 -0800

    Minor update

--- app/oaidp.py
+++ app/oaidp.py
@@ -89,7 +89,7 @@ def collections():
 def collections2():
     codepath = Path(Path(__file__).resolve().parent).joinpath('aspace.py')
     print(codepath)
-    completed_process = subprocess.run(['python', codepath], capture_output=True, text=True)
+    completed_process = subprocess.run(['python', codepath], capture_output=True)
     output = completed_process.stdout
     return render_template('collections.html', done=True, output=output)
 
