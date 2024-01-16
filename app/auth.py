70b1086127751bf82eb359b2e497a33226f8c06a
Stephen Davison <sdavison@caltech.edu>
2024-01-16 15:39:36 -0800

    Add SSO

--- app/auth.py
+++ app/auth.py
@@ -6,6 +6,7 @@ from app.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+'''
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -34,12 +35,19 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+'''
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
+    user_email = authorize_user()
+    if user_email:
+        print(user_email)
+    else:
+        print('None')
+
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
+        #password = request.form['password']
         db = get_db()
         error = None
         user = db.execute(
@@ -50,8 +58,8 @@ def login():
             error = 'Incorrect username.'
         #elif not check_password_hash(user['password'], password):
         #using plain text passwords for now
-        elif user['password'] != password:
-            error = 'Incorrect password.'
+        #elif user['password'] != password:
+        #    error = 'Incorrect password.'
 
         if error is None:
             session.clear()
@@ -88,3 +96,17 @@ def login_required(view):
 
     return wrapped_view
 
+def authorize_user():
+    if request.environ.get("REMOTE_USER", None):
+        # REMOTE_USER is an email address in Shibboleth
+        email_address = request.environ["REMOTE_USER"]
+        # we check the username against our authorized users.csv file
+        query = "SELECT username FROM user WHERE username = ?;"
+        db = get_db()
+        user = db.execute(query, [email_address]).fetchone()[0]
+        if user:
+            return user
+        else:
+            return None
+    else:
+        return None
