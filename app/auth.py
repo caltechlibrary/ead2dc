9a4e20d6040a67aecaf2e4790c4be00e94969af1
Stephen Davison <sdavison@caltech.edu>
2024-01-17 07:57:56 -0800

    Implement SSO

--- app/auth.py
+++ app/auth.py
@@ -6,71 +6,15 @@ from app.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-'''
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("auth.login"))
-
-        flash(error)
-
-    return render_template('auth/register.html')
-'''
-
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     authorize_user()
     return render_template('index.html')
-    '''
-    if request.method == 'POST':
-        username = request.form['username']
-        #password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        #elif not check_password_hash(user['password'], password):
-        #using plain text passwords for now
-        #elif user['password'] != password:
-        #    error = 'Incorrect password.'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
-
-        flash(error)
-
-    return render_template('auth/login.html')
-    '''
 
 @bp.before_app_request
 def load_logged_in_user():
     g.user = session.get('user_id')
+    g.auth = session.get('auth')
 
 @bp.route('/logout')
 def logout():
@@ -90,14 +34,14 @@ def login_required(view):
 def authorize_user():
     # REMOTE_USER is an email address in Shibboleth
     email_address = request.environ["REMOTE_USER"]
-    if '@caltech.edu' in email_address:
-        username = email_address[:email_address.find('@caltech.edu')]
-    else:
-        username = email_address
-    # we check the username against our authorized users
+    username = email_address[:email_address.find('@caltech.edu')]
+    # check the username against authorized users
+    session.clear()
+    session['user_id'] = username
     query = "SELECT username FROM user WHERE username = ?;"
     db = get_db()
     if db.execute(query, [email_address]).fetchone():
-        session.clear()
-        session['user_id'] = username
+        session['auth'] = True
+    else:
+        session['auth'] = False
     return redirect(url_for('index'))
