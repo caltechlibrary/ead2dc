231b5575b8c0be326d2200c61fa4817f3f84f477
Stephen Davison <sdavison@caltech.edu>
2024-11-25 14:43:10 -0800

    print single json

---
+++ api_scripts/printsingleJSON.py
@@ -0,0 +1,11 @@
+secrets = __import__('secrets')
+
+from asnake.client import ASnakeClient
+
+client = ASnakeClient(baseurl = secrets.baseurl,
+                      username = secrets.username,
+                      password = secrets.password)
+
+client.authorize()
+
+print(client.get('repositories/2/digital_objects/8889'))
