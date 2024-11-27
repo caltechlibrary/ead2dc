d64a0823035471a385769cbe908f94da9104bd68
Stephen Davison <sdavison@caltech.edu>
2024-11-27 14:46:58 -0800

    Link DO and AO

--- api_scripts/connectDigitalArchivalObjects.py
+++ api_scripts/connectDigitalArchivalObjects.py
@@ -11,7 +11,7 @@ client.authorize()
 for obj in client.get_paged('repositories/2/digital_objects'):
     linked_instances = obj['linked_instances']
     for linked_instance in linked_instances:
-        if linked_instance[:33] == '/repositories/2/archival_objects/'
-        print(linked_instance['ref'])
+        if linked_instance[:33] == '/repositories/2/archival_objects/':
+            print(linked_instance['ref'])
     
         
