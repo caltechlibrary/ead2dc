d9e3b151668880e98662c77291ae26267b4f2795
Stephen Davison <sdavison@caltech.edu>
2024-11-27 14:25:23 -0800

    Link DO and AO

--- api_scripts/connectDigitalArchivalObjects.py
+++ api_scripts/connectDigitalArchivalObjects.py
@@ -8,9 +8,20 @@ client = ASnakeClient(baseurl = secrets.baseurl,
 
 client.authorize()
 
-n = 0
+n, found, not_found = 0, 0, 0
+
 for obj in client.get_paged('repositories/2/digital_objects'):
     print(obj['uri'])
-    n += 1
-
+    for linked_instance in obj['linked_instances']:
+        if linked_instance['ref'][:33] == '/repositories/2/archival_objects/':
+            print('-->>', linked_instance['ref'])
+            found += 1
+        else:
+            print('not found')
+            not_found += 1
+    
+    print('links found:', found)
+    print('not found:', not_found)
+    print('total:', n)
+          
 print(n)
