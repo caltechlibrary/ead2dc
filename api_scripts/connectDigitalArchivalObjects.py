d5a951d3f76f927f83d2bed69c9e7fa0a090eb50
Stephen Davison <sdavison@caltech.edu>
2024-12-04 15:27:46 -0800

    Test

--- api_scripts/connectDigitalArchivalObjects.py
+++ api_scripts/connectDigitalArchivalObjects.py
@@ -10,12 +10,13 @@ client.authorize()
 
 links = dict()
 
-for obj in client.get_paged('repositories/2/digital_objects'):
+for obj in client.get_paged('/repositories/2/digital_objects'):
     items = list()
     for linked_instance in obj['linked_instances']:
         if linked_instance['ref'][:33] == '/repositories/2/archival_objects/':
-            items.append(linked_instance['ref'][33:])
+            items.append(linked_instance['ref'])
+    links[obj['uri']] = items
 
-print(items)
+print(links)
     
         
