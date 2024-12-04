de9f15accef7d1aef54ff18c9841c5a0a328d4f9
Stephen Davison <sdavison@caltech.edu>
2024-12-04 13:39:16 -0800

    Link DO and AO

--- api_scripts/api-test.py
+++ api_scripts/api-test.py
@@ -8,5 +8,5 @@ client = ASnakeClient(baseurl = secrets.baseurl,
 
 client.authorize()
 
-repos = client.get('/repositories').json()['name']
+repos = client.get('/repositories').json()[0]['name']
 print(repos)
