c4df56cd8ae8fbe7f320774c7efe7769b4d52d7e
Stephen Davison <sdavison@caltech.edu>
2024-12-04 13:41:48 -0800

    Test

--- api_scripts/api-test.py
+++ api_scripts/api-test.py
@@ -8,5 +8,5 @@ client = ASnakeClient(baseurl = secrets.baseurl,
 
 client.authorize()
 
-repos = client.get('/repositories/2').json()
+repos = client.get('/repositories/2/archival_objects').json()[0]
 print(repos)
