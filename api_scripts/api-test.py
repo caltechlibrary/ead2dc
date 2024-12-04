7812635163dd560b8a29b131c59b7106a906bba0
Stephen Davison <sdavison@caltech.edu>
2024-12-04 13:53:33 -0800

    Test

--- api_scripts/api-test.py
+++ api_scripts/api-test.py
@@ -9,10 +9,10 @@ client = ASnakeClient(baseurl = secrets.baseurl,
 client.authorize()
 
 #To get a single object:
-print(client.get('/repositories/2/archival_objects/74627').json())
+print(client.get('/repositories/2/archival_objects/74627').json()['title'])
 
 print('------------------------')
 
 #Tp get all objects:
 for obj in client.get_paged('repositories/2/digital_objects'):
-    print(obj)
+    print(obj['title'])
