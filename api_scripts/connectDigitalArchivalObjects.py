7339423af9e2d08527323b2a34a7431d946bb045
Stephen Davison <sdavison@caltech.edu>
2024-12-06 16:50:37 -0800

    Test

--- api_scripts/connectDigitalArchivalObjects.py
+++ api_scripts/connectDigitalArchivalObjects.py
@@ -7,27 +7,26 @@ client = ASnakeClient(baseurl = secrets.baseurl,
                       password = secrets.password)
 
 client.authorize()
-
-links1, links2 = dict(), dict()
+digital_objects = dict()
 archival_objects = set()
+collections = dict()
 
-coll_not_found1, coll_not_found2 = 0, 0
 for obj in client.get_paged('/repositories/2/digital_objects'):
-    coll1 = obj.get('collection', 'not found')
-    if coll1 == 'not found':
-        coll_not_found1 += 1
     items = set()
+    coll = obj['collection'][0]['ref']
     for linked_instance in obj['linked_instances']:
         if linked_instance['ref'][:33] == '/repositories/2/archival_objects/':
-            coll2 = linked_instance.get('collection', 'not found')
-            if coll2 == 'not found':
-                coll_not_found2 += 1
             items.add(linked_instance['ref'])
             archival_objects.add(linked_instance['ref'])
-    links1[obj['uri']] = items
+    digital_objects[obj['uri']] = items
+    if collections.get(coll):
+        collections[coll] = [digital_objects[obj['uri']]]
+    else:
+        collections[coll].append(digital_objects[obj['uri']])
+    
 
-print(links1)
-
+print(collections)
+'''
 for archival_object in archival_objects:
     links2[archival_object[33:]] = set()
 
@@ -41,8 +40,7 @@ for archival_object_id in links2:
 print(links2)
 
 print('colls not found:', coll_not_found1, coll_not_found2)
-
-    
+''' 
 
 
 
