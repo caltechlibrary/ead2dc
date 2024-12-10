1bbfa575f6d4ee916d6d36c57dbbd304a3c459e6
Stephen Davison <sdavison@caltech.edu>
2024-12-09 22:01:18 -0500

    Test

--- api_scripts/connectDigitalArchivalObjects.py
+++ api_scripts/connectDigitalArchivalObjects.py
@@ -13,7 +13,7 @@ collections = set()
 
 for obj in client.get_paged('/repositories/2/digital_objects'):
     items = set()
-    coll = obj['collection'][0]['ref']
+    coll = obj['collection']
     print(coll)
     for linked_instance in obj['linked_instances']:
         if linked_instance['ref'][:33] == '/repositories/2/archival_objects/':
