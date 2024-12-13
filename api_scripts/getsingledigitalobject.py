ffdc75dd5c33f299a33e29f76616d88454148d56
Stephen Davison <sdavison@caltech.edu>
2024-12-13 09:17:34 -0800

    Test

---
+++ api_scripts/getsingledigitalobject.py
@@ -0,0 +1,16 @@
+secrets = __import__('secrets')
+
+import json
+from asnake.client import ASnakeClient
+
+client = ASnakeClient(baseurl = secrets.baseurl,
+                      username = secrets.username,
+                      password = secrets.password)
+
+client.authorize()
+
+id = '8889'
+
+obj = client.get('/repositories/2/digital_objects/'+id).json()
+
+print(json.dumps(obj, indent=4, sort_keys=True))
