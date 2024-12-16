5a5fe5d506d3387c9668e951b922c071a600c6c4
Stephen Davison <sdavison@caltech.edu>
2024-12-16 13:30:23 -0800

    Test

--- api_scripts/getsingleobject.py
+++ api_scripts/getsingleobject.py
@@ -13,6 +13,8 @@ uri = '/repositories/2/digital_objects/27551'
 
 obj = client.get(f'{uri}?resolve[]=linked_instances').json()
 
+for key in obj.keys:
+    print(key)
 print(json.dumps(obj, indent=4, sort_keys=True))
 
 print()
@@ -21,4 +23,6 @@ uri = '/repositories/2/archival_objects/74627'
 
 obj = client.get(f'{uri}?resolve[]=ancestors').json()
 
+for key in obj.keys:
+    print(key)
 print(json.dumps(obj, indent=4, sort_keys=True))
