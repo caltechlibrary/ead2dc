e16f346e1e185a72307df3d1c99c3ee903a8e347
Stephen Davison <sdavison@caltech.edu>
2024-12-20 16:09:23 -0800

    Create base OAIDP

--- app/oai.py
+++ app/oai.py
@@ -1,11 +1,61 @@
-secrets = __import__('secrets')
-
+import requests, json
+import sqlite3 as sq
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as dom
+from datetime import date, datetime
+from pathlib import Path
 from asnake.client import ASnakeClient
 
+# FUNCTIONS
+
+#read collection info from db
+def read_colls_from_db():
+    connection = sq.connect(dbpath)
+    db = connection.cursor()
+    query = 'SELECT collno, eadurl, colltitle, description FROM collections'
+    colls = db.execute(query).fetchall()
+    db.close()
+    connection.close()
+    return colls
+
+#returns a "pretty" XML string
+def prettify(elem):
+    xml_string = ET.tostring(elem)
+    xml_file = dom.parseString(xml_string)
+    pretty_xml = xml_file.toprettyxml(indent="  ")
+    return pretty_xml
+
+#write time of last update to db
+def update_last_update():
+    # write ISO last update
+    now = datetime.now()
+    last_update = now.isoformat()
+    connection = sq.connect(dbpath)
+    db = connection.cursor()
+    query = 'UPDATE last_update SET dt=? WHERE fn=?;'
+    db.execute(query, [last_update, 'xml'])
+    db.close()
+    connection.commit()
+    connection.close()
+    return
+
+# MAIN
+
+# read config file
+with open(Path(Path(__file__).resolve().parent).joinpath('config.json'), "r") as f:
+    config = json.load(f)
+
+# db location
+dbpath = Path(Path(__file__).resolve().parent).joinpath('../instance/ead2dc.db')
+
+# string form of date to write to each record
+today = date.today().strftime("%Y-%m-%d")
+
+# establish API connection
+secrets = __import__('secrets')
 client = ASnakeClient(baseurl = secrets.baseurl,
                       username = secrets.username,
                       password = secrets.password)
-
 client.authorize()
 
 # initialize collections dictionary
@@ -34,25 +84,69 @@ for obj in client.get_paged('/repositories/2/digital_objects'):
 
 print ('Number of collections with digital objects:', len(collections))
 
-'''
-for collection in collections:
-    print(collection, client.get(f'{collection}').json()['title'])
+colls = read_colls_from_db()
 
-for item in collections_dict.items():
-    print(client.get(f'{item[0]}').json()['title'], len(item[1]))
+# namespace dictionary
+ns = {'': 'urn:isbn:1-931666-22-9', 
+      'xlink': 'http://www.w3.org/1999/xlink',
+      'oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+      'dc': 'http://purl.org/dc/elements/1.1/'}
+ET.register_namespace('', 'http://www.openarchives.org/OAI/2.0/')
+ET.register_namespace('xlink', 'http://www.w3.org/1999/xlink')
+ET.register_namespace('oai_dc', 'http://www.openarchives.org/OAI/2.0/oai_dc/')
+ET.register_namespace('dc', 'http://purl.org/dc/elements/1.1/')
 
-for archival_object in archival_objects:
-    links2[archival_object[33:]] = set()
+# create OAI-PMH XML object
+oaixml = ET.Element('OAI-PMH', {'xmlns': 'http://www.openarchives.org/OAI/2.0/'})
 
-for digital_object, archival_objects in links1.items():
-    for archival_object in archival_objects:
-        links2[archival_object[33:]].add(digital_object[32:])
+# build Identify segment
+Identify = ET.SubElement(oaixml, 'Identify')
+repositoryName = ET.SubElement(Identify, 'repositoryName')
+repositoryName.text = 'Caltech Archives Digital Collections'
+baseURL = ET.SubElement(Identify, 'baseURL')
+baseURL.text = 'http://apps.library.caltech.edu/ead2dc/oai'
+protocolVersion = ET.SubElement(Identify, 'protocolVersion')
+protocolVersion.text = '2.0'
+adminEmail = ET.SubElement(Identify, 'adminEmail')
+adminEmail.text = 'archives@caltech.edu'
+deletedRecord = ET.SubElement(Identify, 'deletedRecord')
+deletedRecord.text = 'no'
+granularity = ET.SubElement(Identify, 'granularity')
+granularity.text = 'YYYY-MM-DD'
 
-for archival_object_id in links2:
-    links2[archival_object_id] = list(links2[archival_object_id])
+# build ListMetadataFormats segment
+ListMetadataFormats = ET.SubElement(oaixml, 'ListMetadataFormats')
+metadataFormat = ET.SubElement(ListMetadataFormats, 'metadataFormat')
+metadataPrefix = ET.SubElement(metadataFormat, 'metadataPrefix')
+metadataPrefix.text = "oai_dc"
+schema = ET.SubElement(metadataFormat, 'schema')
+schema.text = "http://www.openarchives.org/OAI/2.0/oai_dc.xsd"
+metadataNamespace = ET.SubElement(metadataFormat, 'metadataNamespace')
+metadataNamespace.text = "http://www.openarchives.org/OAI/2.0/oai_dc/"
 
-#print(links2)
+# build ListSets segment
+ListSets = ET.SubElement(oaixml, 'ListSets')
+                         
+for coll in colls:
+    set = ET.SubElement(ListSets, 'set')
+    setSpec = ET.SubElement(set, 'setSpec')
+    setSpec.text = coll[0]
+    setName = ET.SubElement(set, 'setName')
+    setName.text = coll[2]
+    setDescription = ET.SubElement(ET.SubElement(ET.SubElement(
+        set, 'setDescription'), 'oai_dc', {'xmlns:oai_dc': 'http://www.openarchives.org/OAI/2.0/oai_dc/',
+                                           'xmlns:dc': 'http://purl.org/dc/elements/1.1/'}), 'dc:description')
+    setDescription.text = coll[3]
 
-print('colls not found:', coll_not_found1, coll_not_found2)
- 
-'''
+no_records = 0
+
+dao_count = 0
+
+#write to disk
+fileout = Path(Path(__file__).resolve().parent).joinpath('../xml/caltecharchives.xml')
+with open(fileout, 'w') as f:
+    f.write(prettify(oaixml))
+
+update_last_update()
+
+print(dao_count)
