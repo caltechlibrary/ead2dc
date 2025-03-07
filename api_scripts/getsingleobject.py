c898d9100b4e48fac6034072753ea3d3b4fc843f
Stephen Davison <sdavison@caltech.edu>
2025-03-07 14:51:50 -0800

    Test

--- api_scripts/getsingleobject.py
+++ api_scripts/getsingleobject.py
@@ -11,19 +11,48 @@ client.authorize()
 
 args = sys.argv[1:]
 
+proceed = True
+
+if len(args) != 3:
+    print('Invalid number of arguments. Must be 3.')
+    proceed = False
+else:
+    if args[1] not in ['digital', 'archival', 'resource']:
+        print('Invalid type. Must be "digital", "archival", or "resource"')
+        proceed = False
+    try:
+        int(args[2])
+    except:
+        print('Invalid ID. Must be an integer.')
+        proceed = False
+
+if proceed:
+    if args[1] == 'digital':
+        uri = f'/repositories/2/digital_objects/{args[2]}'
+    elif args[1] == 'archival':
+        uri = f'/repositories/2/archival_objects/{args[2]}'
+    elif args[1] == 'resource':
+        uri = f'/repositories/2/resources/{args[2]}'
+    obj = client.get(f'{uri}').json()
+    print(json.dumps(obj, indent=4, sort_keys=True))
+
+'''
+
 if args[1] == 'digital':
     uri = f'/repositories/2/digital_objects/{args[2]}'
 elif args[1] == 'archival':
     uri = f'/repositories/2/archival_objects/{args[2]}'
-else args[1] == 'resource':
+elif args[1] == 'resource':
     uri = f'/repositories/2/resources/{args[2]}'
+else:
+    print('Invalid type. Must be "digital", "archival", or "resource"')
 
 uri = '/repositories/2/digital_objects/8889'
 #obj = client.get(f'{uri}?resolve[]=linked_instances').json()
 obj = client.get(f'{uri}').json()
 print(json.dumps(obj, indent=4, sort_keys=True))
 
-'''
+
 print()
 
 uri = '/repositories/2/digital_objects/27551'
