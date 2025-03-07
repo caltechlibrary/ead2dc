543b716fd30232938b3167febcf516349b9b30a4
Stephen Davison <sdavison@caltech.edu>
2025-03-07 15:35:29 -0800

    Test

--- api_scripts/getsingleobject.py
+++ api_scripts/getsingleobject.py
@@ -39,42 +39,8 @@ if proceed:
 
 '''
 
-if args[1] == 'digital':
-    uri = f'/repositories/2/digital_objects/{args[2]}'
-elif args[1] == 'archival':
-    uri = f'/repositories/2/archival_objects/{args[2]}'
-elif args[1] == 'resource':
-    uri = f'/repositories/2/resources/{args[2]}'
-else:
-    print('Invalid type. Must be "digital", "archival", or "resource"')
-
 uri = '/repositories/2/digital_objects/8889'
-#obj = client.get(f'{uri}?resolve[]=linked_instances').json()
-obj = client.get(f'{uri}').json()
-print(json.dumps(obj, indent=4, sort_keys=True))
-
-
-print()
-
 uri = '/repositories/2/digital_objects/27551'
-#obj = client.get(f'{uri}?resolve[]=linked_instances').json()
-obj = client.get(f'{uri}').json()
-for key in obj.keys():
-    print(key)
-print(json.dumps(obj, indent=4, sort_keys=True))
-
-print()
-
 uri = '/repositories/2/archival_objects/74627'
-obj = client.get(f'{uri}?resolve[]=ancestors').json()
-obj = client.get(f'{uri}').json()
-for key in obj.keys():
-    print(key)
-print(json.dumps(obj, indent=4, sort_keys=True))
-
-print()
-
 uri = '/repositories/2/resources/219'
-obj = client.get(f'{uri}').json()
-print(json.dumps(obj, indent=4, sort_keys=True))
 '''
