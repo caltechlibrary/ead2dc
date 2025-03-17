b40b603fe960e1d852ef0783283f5d4ba9b6f9f2
Stephen Davison <sdavison@caltech.edu>
2025-03-17 16:04:02 -0700

    Test

--- api_scripts/getsingleobject.py
+++ api_scripts/getsingleobject.py
@@ -16,22 +16,26 @@ parser.add_argument('-i', '--identifier', default='')
 
 # Read arguments from command line
 args = parser.parse_args()
+object_type = args.object_type
+identifier = int(args.identifier)
 
 proceed = True
 
-if not isinstance(args.identifier, int) or args.object_type not in ['digital', 'archival', 'resource']:
-    print(args.identifier, args.object_type)
-    print(type(args.identifier), type(args.object_type))
-    print('Invalid arguments. Must provide object type ("digital", "archival", or "resource") and identifier (integer).')
+if object_type not in ['digital', 'archival', 'resource']:
+    print('Invalid arguments. Must provide object type ("digital", "archival", or "resource").')
+    proceed = False
+
+if identifier == 0:
+    print('Invalid arguments. Must provide an identifier.')
     proceed = False
 
 if proceed:
-    if args[0] == 'digital':
-        uri = f'/repositories/2/digital_objects/{args[1]}'
-    elif args[0] == 'archival':
-        uri = f'/repositories/2/archival_objects/{args[1]}'
-    elif args[0] == 'resource':
-        uri = f'/repositories/2/resources/{args[1]}'
+    if object_type == 'digital':
+        uri = f'/repositories/2/digital_objects/{identifier}'
+    elif object_type == 'archival':
+        uri = f'/repositories/2/archival_objects/{identifier}'
+    elif object_type == 'resource':
+        uri = f'/repositories/2/resources/{identifier}'
     obj = client.get(f'{uri}?resolve[]=linked_instances').json()
     print(json.dumps(obj, indent=4, sort_keys=True))
 
