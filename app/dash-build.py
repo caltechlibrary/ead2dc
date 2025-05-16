cd4da6961222b394ef50ace047151b8844837d06
Stephen Davison <sdavison@caltech.edu>
2025-05-16 11:05:30 -0700

    Dash

--- app/dash-build.py
+++ app/dash-build.py
@@ -18,11 +18,11 @@ from asnake.client import ASnakeClient
 
 # MAIN
 
-start = time.time()
+#start = time.time()
 
 # db location
-print('Reading database location...')
-dbpath = Path(Path(__file__).resolve().parent).joinpath('../instance/ead2dc.db')
+#print('Reading database location...')
+#dbpath = Path(Path(__file__).resolve().parent).joinpath('../instance/ead2dc.db')
 
 # establish API connection
 print('Establishing API connection...')
@@ -32,22 +32,17 @@ client = ASnakeClient(baseurl = secrets.baseurl,
                       password = secrets.password)
 client.authorize()
 
-# initialize collections dictionary
-# this dictionary references archival objects with related digital objects
-collections_dict = dict()
-
-# initialize collection and archival object sets
-# contain collections and archival objects with related digital objects
-collections, archival_objects = set(), set()
+# initialize resources, archival object, and digital object sets
+resources, accessions, archival_objects, digital_objects = set(), set(), set(), set()
 
 dao_count = 0
 
 print('Finding digital content...')
 
-
 numb_do = 0 # number of digital objects
 numb_ao = 0 # number of archival objects
 numb_ac = 0 # number of accessions
+numb_re = 0 # number of resources
 
 linked_dict = dict()
 collections_dict = dict()
@@ -71,7 +66,17 @@ print(dict(sorted(linked_dict.items())), 'digital objects with linked instances'
 print(dict(sorted(collections_dict.items())), 'digital objects with collections')
 
 for obj in client.get_paged('/repositories/2/digital_objects'):
+    digital_objects.add(obj.get('uri'))
 
+for obj in client.get_paged('/repositories/2/archival_objects'):
+    archival_objects.add(obj.get('uri'))
+
+for obj in client.get_paged('/repositories/2/resources'):
+    resources.add(obj.get('uri'))
+
+for obj in client.get_paged('/repositories/2/accessions'):
+    accessions.add(obj.get('uri'))
+'''
     n = len(obj.get('linked_instances', []))
     if n > 2:
         print(obj.get('uri'), 'is linked to', n, 'instances:')
@@ -83,3 +88,8 @@ for obj in client.get_paged('/repositories/2/digital_objects'):
         print(obj.get('uri'), 'is associated with', m, 'collections:')
         for o in obj.get('collection', []):
             print('                    ', o.get('ref'))
+'''
+print('Found', len(digital_objects), 'digital objects')
+print('Found', len(archival_objects), 'archival objects')
+print('Found', len(resources), 'resources')
+print('Found', len(accessions), 'accessions')
