552ba8170ae27e3e01963628576089bf75a3b386
Stephen Davison <sdavison@caltech.edu>
2025-05-15 17:13:23 -0700

    Stats

--- app/dash-build.py
+++ app/dash-build.py
@@ -67,5 +67,5 @@ for obj in client.get_paged('/repositories/2/digital_objects'):
         collections_dict[m] = 1
 
 print(numb_do, 'digital objects found')
-print(linked_dict)
-print(collections_dict)
+print(dict(sorted(dict.items())), 'digital objects with linked instances')
+print(dict(sorted(collections_dict.items())), 'digital objects with collections')
