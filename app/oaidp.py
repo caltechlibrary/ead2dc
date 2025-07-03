ebb65738f00279b1787cd612230ddd0b9e3287ef
Stephen Davison <sdavison@caltech.edu>
2025-07-03 09:34:13 -0700

    Update

--- app/oaidp.py
+++ app/oaidp.py
@@ -260,11 +260,9 @@ def records():
                     #df = df.rename(columns=lambda x: x.replace('.', '_'))
                     csv_filename = Path(Path(__file__).resolve().parent).joinpath(g.user + '_' + recordtype + recordid + '.csv')
                     df.to_csv(csv_filename, index=False)
-                    send_file(csv_filename, as_attachment=True)
-                    return render_template('records.html')
+                    return send_file(csv_filename, as_attachment=False)
                 else:
-                    send_file(json_filename, as_attachment=True)
-                    return render_template('records.html')
+                    return send_file(json_filename, as_attachment=False)
         else:
             return render_template('records.html', 
                                    error='No record ID provided.')
