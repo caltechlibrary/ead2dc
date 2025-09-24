913077f622599e6c57e5c44c22ba651b09aabbe3
Stephen Davison <nosivads@gmail.com>
2025-09-24 16:37:33 -0700

    Subjects

--- app/xml-build.py
+++ app/xml-build.py
@@ -20,8 +20,6 @@ from datetime import date, datetime
 from pathlib import Path
 from urllib.parse import urlparse
 
-from aspace import get_subjects, get_extents, get_dates
-
 from asnake.client import ASnakeClient
 
 # FUNCTIONS
@@ -33,6 +31,45 @@ def prettify(elem):
     pretty_xml = xml_file.toprettyxml(indent="  ")
     return pretty_xml
 
+def get_ids(category):
+    client.authorize()
+    id_list = list()
+    for obj in client.get_paged('/repositories/2/'+category):
+        id = obj['uri'][obj['uri'].rfind('/')+1: ]
+        id_list.append((id, obj['uri']))
+    return id_list
+
+def get_json(category, id):
+    client.authorize()
+    uri = '/repositories/2/'+category+'/'+id
+    obj = client.get(uri)
+    return obj.json()
+
+def get_subjects(category, id):
+    subjects = list()
+    client.authorize()
+    obj = get_json(category, id)
+    for ref in obj.get('subjects', []):
+        uri = ref.get('ref', None)
+        if uri:
+            subjects.append(client.get(uri).json()['title'])
+    return subjects
+
+def get_dates(category, id):
+    dates = list()
+    client.authorize()
+    obj = get_json(category, id)
+    for date in obj.get('dates', []):
+        dates.append(date.get('expression', ''))
+    return dates
+
+def get_extents(category, id):
+    extents = list()
+    client.authorize()
+    obj = get_json(category, id)
+    for extent in obj.get('extents', []):
+        extents.append(extent.get('number', '') + ' ' + extent.get('extent_type', ''))
+    return extents
 
 # MAIN
 
