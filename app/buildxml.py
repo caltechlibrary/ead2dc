5658b5b29634ce1c68d93c0239856d3a011e2347
Stephen Davison <nosivads@gmail.com>
2025-09-25 18:22:28 -0700

    Ancestors

--- app/buildxml.py
+++ app/buildxml.py
@@ -47,9 +47,9 @@ def get_ancestors(category, id):
             #print('this is _resolved:', a['_resolved'])
             if a['_resolved'].get('title'):
                 title = a['_resolved']['title']
-                print('title:', title)
+                #print('title:', title)
                 #level = a.get['level']
-                print('level:', level)
+                #print('level:', level)
                 ancestors.append((title, level))
     #print(ancestors)
     return ancestors
