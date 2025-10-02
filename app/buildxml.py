8bae7c06b823271b9ca5190728d66f44ca194722
Stephen Davison <nosivads@gmail.com>
2025-10-01 23:02:28 -0700

    Version 1

--- app/buildxml.py
+++ app/buildxml.py
@@ -372,7 +372,7 @@ for coll in colls:
     dao_dict[setid] = dict() # initialize dictionary for collection's statistics
 
     #temp
-    j=0
+    #j=0
 
     if collections_dict.get(setid):
 
@@ -561,9 +561,9 @@ for coll in colls:
                 dao_skipped += 1
 
             #temp
-            j += 1
-            if j >= 20:
-                break
+            #j += 1
+            #if j >= 20:
+            #    break
 
         print('>', recs_created, 'records created')
         print('>', recs_skipped, 'records skipped')
