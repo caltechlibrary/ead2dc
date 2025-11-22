0b06a8c7b12dac11150cd9fceffdc4a8c15021e0
Stephen Davison <nosivads@gmail.com>
2025-11-21 22:52:22 -0800

    Update

--- app/buildxml.py
+++ app/buildxml.py
@@ -490,7 +490,7 @@ for coll in colls:
                     #    and typ == 'resource' \
                     #    and file_version.get('use_statement', 'ok') not in ['image-thumbnail', 'URL-Redirected']:
 
-                    if file_version.get('publish', True:)
+                    if file_version.get('publish', True):
                         file_uri_list.append(file_version['file_uri'])
 
                 # temp
