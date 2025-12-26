fd71c771c558f7458283f35ab6af74cbb0792f79
Stephen Davison <nosivads@gmail.com>
2025-12-25 22:10:00 -0800

    Update

---
+++ app/secrets_template.py
@@ -0,0 +1,8 @@
+# model for secrets.py file
+# replace:
+# baseurl: base url for OAI requests
+baseurl='<baseurl>'
+# username: ArchivesSpace API username
+username='<username>'
+# password: ArchivesSpace API password
+password='<password>'
